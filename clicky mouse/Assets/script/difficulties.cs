6bdbaf439b03e62b54038ba9b854cb4975be6bd0
doris624 <doris5730@gmail.com>
2021-05-21 19:21:44 +0800

    add empty project

---
+++ clicky mouse/Assets/script/difficulties.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class difficulties : MonoBehaviour
+{
+    private Button button;
+    private gameManager gameManager;
+    public int difficulty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(setDifficulty);
+        gameManager = GameObject.Find("gamemanager").GetComponent<gameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void setDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
